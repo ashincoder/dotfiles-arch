dc0d9cbdd62f3a1e3de24b76ddad99b3fba15637
ashincoder <ashinant15@gmail.com>
2021-06-16 19:12:34 +0530

    deleted emacs

--- .config/emacs/.local/elpa/no-littering-20210602.1139/no-littering-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from no-littering.el  -*- no-byte-compile: t -*-
-(define-package "no-littering" "20210602.1139" "help keeping ~/.emacs.d clean" '((cl-lib "0.5")) :commit "e521d261d780f3a69344b80ed5d804c6bb140e78" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/emacscollective/no-littering")
